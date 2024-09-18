1667cc868e3e323a93a10ebe440c5bafec158322
Esotericlassie <136794995+Esotericlassie@users.noreply.github.com>
2024-09-18 20:39:07 +0530

    Updated Graph.tsx and DataManipulator.ts to reflect new schema and data processing requirements

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -10,50 +10,60 @@ interface IProps {
 
 interface PerspectiveViewerElement extends HTMLElement {
   load: (table: Table) => void,
+  setAttribute: (name: string, value: string) => void,
 }
+
 class Graph extends Component<IProps, {}> {
   table: Table | undefined;
-
-  render() {
-    return React.createElement('perspective-viewer');
-  }
 
   componentDidMount() {
     // Get element from the DOM.
     const elem = document.getElementsByTagName('perspective-viewer')[0] as unknown as PerspectiveViewerElement;
 
     const schema = {
-      stock: 'string',
-      top_ask_price: 'float',
-      top_bid_price: 'float',
+      ratio: 'float',
+      upper_bound: 'float',
+      lower_bound: 'float',
+      trigger_alert: 'string',
+      price_abc: 'float',
+      price_def: 'float',
       timestamp: 'date',
     };
 
     if (window.perspective && window.perspective.worker()) {
       this.table = window.perspective.worker().table(schema);
     }
-    if (this.table) {
+
+    if (this.table && elem) {
       // Load the `table` in the `<perspective-viewer>` DOM reference.
       elem.load(this.table);
       elem.setAttribute('view', 'y_line');
-      elem.setAttribute('column-pivots', '["stock"]');
       elem.setAttribute('row-pivots', '["timestamp"]');
-      elem.setAttribute('columns', '["top_ask_price"]');
+      elem.setAttribute('columns', '["ratio", "upper_bound", "lower_bound", "trigger_alert"]');
       elem.setAttribute('aggregates', JSON.stringify({
-        stock: 'distinctcount',
-        top_ask_price: 'avg',
-        top_bid_price: 'avg',
+        ratio: 'avg',
+        upper_bound: 'avg',
+        lower_bound: 'avg',
+        trigger_alert: 'first',
+        price_abc: 'avg',
+        price_def: 'avg',
         timestamp: 'distinct count',
       }));
+
+      // Initialize the table with the initial data
+      this.table.update(DataManipulator.generateRow(this.props.data));
     }
   }
 
-  componentDidUpdate() {
-    if (this.table) {
-      this.table.update(
-        DataManipulator.generateRow(this.props.data),
-      );
+  componentDidUpdate(prevProps: IProps) {
+    if (this.table && this.props.data !== prevProps.data) {
+      // Only update the table if the data has changed
+      this.table.update(DataManipulator.generateRow(this.props.data));
     }
+  }
+
+  render() {
+    return React.createElement('perspective-viewer');
   }
 }
 
